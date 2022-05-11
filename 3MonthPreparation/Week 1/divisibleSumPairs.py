981df366900e5179ba546aa01dcc517c68b9f753
Jeffrey Ganulin <45239282+jtganulin@users.noreply.github.com>
2022-05-11 19:41:01 -0400

    Cleaned up divisibleSumPairs
    Cleaned up this challenge solution and updated gitignore

--- 3MonthPreparation/Week 1/divisibleSumPairs.py
+++ 3MonthPreparation/Week 1/divisibleSumPairs.py
@@ -1,21 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'divisibleSumPairs' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER k
-#  3. INTEGER_ARRAY ar
-#
-
 def divisibleSumPairs(n, k, ar):
     # Write your code here
     ar = sorted(ar)
