eb5255e10787e85a5cbcfb49bc0d302da117fc90
Jeffrey Ganulin <45239282+jtganulin@users.noreply.github.com>
2022-06-01 16:04:55 -0400

    Added the rest of Week 3's solutions.

---
+++ 3MonthPreparation/Week 3/drawingBook.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-drawing-book/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+
+def pageCount(n, p):
+    # Write your code here
+    return min([p//2, n//2 - p//2])
+
---
+++ 3MonthPreparation/Week 3/migratoryBirds.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+#https://www.hackerrank.com/challenges/three-month-preparation-kit-migratory-birds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-three
+
+def migratoryBirds(arr):
+    # Write your code here
+    freq = [0] * len(arr)
+    for el in arr:
+        freq[el] += 1
+
+    posMax = []
+    maxEl = max(freq)
+    for i in range(len(freq)):
+        if freq[i] == maxEl:
+            posMax.append(i)
+
+    return min(posMax)
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
